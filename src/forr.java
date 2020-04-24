506b5bfe672fccb815c943903979e825cb53d5d2
zaky <mzaky2678@gmail.com>
2020-04-24 14:32:38 +0800

    forr

---
+++ src/forr.java
@@ -0,0 +1,17 @@
+public class forr {
+    public static void main(String[]args) {
+        int isigelas = 0;
+        int penuh = 12;
+
+        System.out.println("isi gelas sekarang :" + isigelas + "m1");
+        System.out.println("isi gelas ketika penuh :" + penuh + "m1");
+
+        for (isigelas= 0; isigelas <= penuh ; isigelas++){
+            System.out.println("sedang mengisi gelas..");
+            System.out.println("isi gelas sekarang : " + isigelas + "m1");
+        }
+
+        System.out.println("finale: isi gelas sekarang :" + isigelas + "m1");
+
+    }
+}
