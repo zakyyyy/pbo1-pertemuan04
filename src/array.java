99d81a6d8bfceb3cd7f16436ffdc94e35b3172f1
zaky <mzaky2678@gmail.com>
2020-04-24 15:03:10 +0800

    array

---
+++ src/array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[]args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "pemrograman berbasis objek 1";
+        mataKuliah[1] = "visual 2";
+        mataKuliah[2] = "rekayasa perangkat lunak";
+        mataKuliah[3] = "organisasi komputer";
+        mataKuliah[4] = "pemrograman web 2";
+    }
+}
