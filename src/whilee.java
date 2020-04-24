c85c1bdda9b78ecf1e515beec9365b6faf894047
zaky <mzaky2678@gmail.com>
2020-04-24 14:09:58 +0800

    whilee

---
+++ src/whilee.java
@@ -0,0 +1,21 @@
+public class whilee {
+    public static void main(String[]args) {
+        int isigelas = 0;
+        int penuh = 10;
+
+        System.out.println("isi gelas sekarang :" + isigelas +"m1");
+        System.out.println("isi gelas ketika penuh :" + penuh +"m1");
+
+        while (isigelas != penuh){
+            isigelas++;
+            System.out.println("sedang mengisi gelas..");
+            System.out.println("isi gelas sekarang : " +isigelas+"m1");
+        }
+
+        System.out.println("finale: isi gelas sekarang :" +isigelas+"m1");
+    }
+
+
+
+
+}
