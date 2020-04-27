272964fdf0e7816c5e977941bcad837bdb4c9a9f
zaky <mzaky2678@gmail.com>
2020-04-27 10:38:02 +0800

    kasus2

---
+++ src/kasus2.java
@@ -0,0 +1,19 @@
+public class kasus2 {
+    public static void main(String[]args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+        int nilai[]= {50,60,70,80,90,100};
+
+        mataKuliah[0] = "pemrograman berbasis objek 1";
+        mataKuliah[1] = "visual 2";
+        mataKuliah[2] = "rekayasa perangkat lunak";
+        mataKuliah[3] = "organisasi komputer";
+        mataKuliah[4] = "pemrograman web 2";
+
+        System.out.println("NAMA : MUHAMMAD ZAKY");
+        System.out.println("NPM : 18630584");
+        System.out.println(mataKuliah[1]);
+        System.out.println("NILAI ANDA HARI INI");
+        System.out.println(nilai[5]);
+    }
+}
